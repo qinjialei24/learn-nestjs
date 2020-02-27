8ff825d8c22276b52afc8f6ce18fbbb0803a5ba9
qinjialei <jialei.qin@thoughtworks.com>
2020-02-27 08:24:08 +0800

    1

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiPropertyOptional, ApiProperty } from '@nestjs/swagger';
 
 class CreatePostDto {
@@ -32,11 +32,19 @@ export class PostsController {
     return body
   }
 
-  @Get(':id')
-  detail() {
+  @Put(':id')
+  @ApiOperation({ summary: '帖子详情' })
+  detail(@Param('id') id: string, @Body() body: CreatePostDto) {
     return {
-      id: 1,
-      title: '111'
+      success: true
+    }
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '删除帖子' })
+  remove(@Param('id') id: string) {
+    return {
+      success: true
     }
   }
 
