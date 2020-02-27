5faade34f85eab3927334559bfb91f7c9ff053a3
qinjialei <jialei.qin@thoughtworks.com>
2020-02-27 08:49:33 +0800

    add mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as mongoose from "mongoose";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  mongoose.connect('mongodb://localhost/nest-blog-api', {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+  })
 
   const options = new DocumentBuilder()
     .setTitle('博客api')
